3fd28999a8ca33fa35a34d1a7362479678b20220
Harani1606 <haranijm16@gmail.com>
2026-07-12 19:24:43 +0530

    Added Exchange catalog and products

--- script.js
+++ script.js
@@ -17,3 +17,93 @@ const categoryList = document.querySelector(".category-list");
 categoryBtn.addEventListener("click",()=>{
     categoryList.classList.toggle("active");
 });
+
+const slides=document.querySelectorAll(".slide");
+
+const dots=document.querySelectorAll(".dot");
+
+let current=0;
+let slideInterval;
+
+function startAutoSlide() {
+
+    slideInterval = setInterval(nextSlide, 5000);
+
+}
+
+function resetAutoSlide() {
+
+    clearInterval(slideInterval);
+
+    startAutoSlide();
+
+}
+
+function showSlide(index){
+
+    slides.forEach(slide=>slide.classList.remove("active"));
+
+    dots.forEach(dot=>dot.classList.remove("active"));
+
+    slides[index].classList.add("active");
+
+    dots[index].classList.add("active");
+
+}
+
+function nextSlide(){
+
+    current++;
+
+    if(current>=slides.length){
+
+        current=0;
+
+    }
+
+    showSlide(current);
+
+}
+
+function prevSlide(){
+
+    current--;
+
+    if(current<0){
+
+        current=slides.length-1;
+
+    }
+
+    showSlide(current);
+
+}
+
+document.querySelector(".next").onclick=()=>{
+
+    nextSlide();
+    resetAutoSlide();
+
+};
+
+document.querySelector(".prev").onclick=()=>{
+
+    prevSlide();
+    resetAutoSlide();
+};
+
+dots.forEach((dot,index)=>{
+
+    dot.onclick=()=>{
+
+        current=index;
+
+        showSlide(current);
+
+        resetAutoSlide();
+
+    };
+
+});
+
+startAutoSlide();
